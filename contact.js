96886d5c61ff89c7eea059779ee0366014b0f43f
tmatabane <thatomatabane241@hotmail.com>
2023-02-28 16:03:04 +0200

    intergrated contact.js with contact.html

---
+++ contact.js
@@ -0,0 +1,16 @@
+// Nav bar
+document.querySelector(".nav0").textContent = "Home";
+document.querySelector(".nav1").textContent = "About";
+document.querySelector(".nav2").textContent = "Contact Me";
+
+// main
+
+document.querySelector(".heading").textContent = "Contact Me"
+
+document.querySelector(".name").textContent = "Name and Surname:";
+
+document.querySelector(".email").textContent = "Email";
+
+document.querySelector(".msg").textContent = "Message";
+
+document.querySelector(".btn").textContent = "Submit";
