70c21a33797f918b185ffe49af04490a157b25e2
tmatabane <thatomatabane241@hotmail.com>
2023-02-28 15:51:46 +0200

    about.js has been intergrated with about.html

---
+++ about.js
@@ -0,0 +1,42 @@
+// Nav bar
+document.querySelector(".nav0").textContent = "Home";
+document.querySelector(".nav1").textContent = "About";
+document.querySelector(".nav2").textContent = "Contact Me";
+
+// main
+
+document.querySelector("#qoute").innerHTML = "<h1>" + "If You Have A Goal. Then Go Do It.<br> If You Are Scared. Then Go Do It Scared." +"</h1>";
+
+document.querySelector(".title1").textContent = "Employment";
+
+document.querySelector(".header1").innerHTML = "<th>" + "Intern Software Engineer, WeThinkCode" +"</th>"+
+"<th>" + "MAY 2021 - JUL 201" + "</th>";
+
+document.querySelector(".info1").innerHTML = "Worked in a team of 6 to create a web based tutorial for new " + "<span class='emphasize'>" +"<a href='#wethinkcode'>" + "WeThinkCode"+"</a>"+"</span>"+" students who haven’t been exposed to Slack and Google suite apps (such as Gmail , Google drive). The tutorial consisted of summarized notes and amultiple choice quiz at the end.We used the following technologies: CSS, HTML, Javascript and Miro(for planning out thedesign).Worked in a team of 6 to create a web based tutorial for new WeThinkCode students who haven’t been exposed to Slack and Google suite apps ( such as Gmail , Google drive). The tutorial consisted of summarized notes and a multiple choice quiz at the end. We used the following technologies: CSS, HTML, Javascript and Miro(for planning out the design)."
++ "<br>" + "<span class='emphasize'>" + "<a href='#skill'>"+ "Technology " + "</a>" +":"+"</span>" + "HTML · Cascading Style Sheets (CSS) · Miro Collaboration Platform · JavaScript";
+
+document.querySelector(".title2").textContent = "Education";
+
+document.querySelector(".heading2").innerHTML = "<tr>"+ "<th>" +"University Of Limpopo"+"</th>"+"<th>"+"2019 - 2020"+"</th>"+"</tr>";
+
+document.querySelector(".info2").innerHTML = "<tr>"+"<td>" +"Mathematics And Computer Science"+"</td>"+"<td>" +"Computer Science with the following as joint majors: Applied Mathematics, Mathematics,  Statistics, and Operation Research." +"</td>"+"</tr>"
+
+document.querySelector(".heading3").innerHTML = "<tr>"+"<th id='wethinkcode'>"+ "WeThinkCode"+"</th>"+"<th>" +"2020 - 2022"+"</th>"+"</tr>";
+
+document.querySelector(".info3").innerHTML = "<tr>"+"<td>"+"Software Engineer"+"</td>"+"<td>"+"Java - Flutter - Software Development - Python - Front-End Development"+"</td>"+"</tr>";
+
+document.querySelector(".title3").textContent = "Skills";
+
+document.querySelector(".heading4").innerHTML = "<tr>"+"<th>"+"Programming Language"+"</th>"+"<th>"+"Level"+"</th>"+"</tr>";
+
+document.querySelector(".info4").innerHTML = "<tr>"+"<td>"+"Python"+"</td>"+"<td>"+"Semi-intermediate"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Java"+"</td>"+"<td>"+"Semi-intermediate"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Javascript"+"</td>"+"<td>"+"Semi-intermediate"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Flutter & Dart"+"</td>"+"<td>"+"Beginner"+"</td>"+"</tr>";
+
+document.querySelector(".heading5").innerHTML ="<tr>"+"<th>"+"Reading"+"</th>"+"<th>"+"Gaming"+"</th>"+"</tr>";
+
+document.querySelector(".info5").innerHTML = "<td>"+"An avid reader of all genres (a bit hesitant on romance novels, but coming around to it)."+"<td>"+"I game here and there, but mostly a game named"+"<span class='emphasize'>"+"<a target='_blank href='https://play.google.com/store/apps/details?id=com.mobile.legends&hl=en&gl=VN'>"+"Mobile Legends"+"</a>"+"</span>"+"</td>";
+
+document.querySelector(".heading6").innerHTML = "<tr>"+"<th>"+"Language"+"</th>"+"<th>"+"Proficiency"+"</th>"+"</tr>";
+
+document.querySelector(".info6").innerHTML = "<tr>"+"<td>"+"English"+"</td>"+"<td>"+"Native Speaker"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Northern Sotho"+"</td>"+"<td>"+"Native Speaker"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Xhosa"+"</td>"+"<td>"+"Highly proficient"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Setswana"+"</td>"+"<td>"+"Highly proficient"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Zulu"+"</td>"+"<td>"+"Highly proficient"+"</td>"+"</tr>"+"<tr>"+"<td>"+"Afrikaans"+"</td>"+"<td>"+"Very good command"+"</td>"+"</tr>";
+
+document.querySelector(".title4").textContent = "Language Proficiency";
