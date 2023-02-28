c7a93ffafcd3da4be21c3a67736a8df5476518bd
tmatabane <thatomatabane241@hotmail.com>
2023-02-28 14:34:59 +0200

    index.js integrated with index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+// Nav bar
+document.querySelector(".nav0").textContent = "Home";
+document.querySelector(".nav1").textContent = "About";
+document.querySelector(".nav2").textContent = "Contact Me";
+
+// Main
+document.querySelector(".coffeeQoute").textContent = '"Programmer - An organism that turns coffee into code."';
+
+document.querySelector("#aboutMe").innerHTML = "<h1>" +"Hi, I am Thato......." + "</h1>"+
+"<p>" + "an aspiring software engineer, who has has a goal to turn into the mythical 10x developer." + " <br>" + "I have also taken a huge interest in Web development lately"+
+    "<br>" + "and now I am in the Code Tribe bootcamp....whoop! whoop!"+"</p>"
+
+document.querySelector(".location_info").innerHTML = "You can usually find me at a Mugg & Bean restuarant, with a cup of tea or coffee." + "<br>" + 
+"So feel free to drop by, join with a coffee and debate about code." + "<br>" +"Alternatively you can also reach me on social platforms.";
